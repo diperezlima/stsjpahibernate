dc0d891becdad378084b554fbfa5910bf46639cd
diperezlima <dperezlima@gmail.com>
2022-09-20 14:14:20 -0300

    User entity and resource

---
+++ src/main/java/com/panzaprende/cursojava/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.panzaprende.cursojava.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.panzaprende.cursojava.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Diego", "diego@gmail.com", "3123323", "1234");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
